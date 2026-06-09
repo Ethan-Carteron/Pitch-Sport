896e8203fe4c5cfa712a7cb2a37513ae44a2fbbd
EthanCarteron <ethan.carteron@gmail.com>
2026-06-09 15:50:41 +0200

    fix: restore layout and remove inline CSS on stats page
    Fixed block name to restore top/sidebar. Cleaned up inline CSS in the twig template by moving it to app.css.

--- src/Controller/Stat/StatController.php
+++ src/Controller/Stat/StatController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller\Stat;
 
-use App\Entity\Player;
+use App\Repository\PlayerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,10 +12,16 @@ final class StatController extends AbstractController
     #[Route('/stat/{uid}', name: 'app_stat')]
     public function index
     (
-        uuid $uid,
-        Player $player
+        string           $uid,
+        PlayerRepository $playerRepository
     ): Response
     {
+        $player = $playerRepository->findOneBy(['uid' => $uid]);
+
+        if (!$player) {
+            throw $this->createNotFoundException('Joueur introuvable.');
+        }
+
         return $this->render('stat/index.html.twig', [
             'player' => $player,
         ]);
