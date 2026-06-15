5ea6491a573bc27c3cb5663731e8af583b4510cb
EthanCarteron <ethan.carteron@gmail.com>
2026-06-15 16:07:24 +0200

    fix: set cache and log directories to /tmp for Vercel read-only filesystem

--- api/index.php
+++ api/index.php
@@ -2,4 +2,5 @@
 // api/index.php
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = 'prod';
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = 0;
+$_SERVER['VERCEL'] = $_ENV['VERCEL'] = 1;
 return require __DIR__ . '/../public/index.php';
--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,20 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getCacheDir(): string
+    {
+        if (isset($_SERVER['VERCEL']) || isset($_ENV['VERCEL'])) {
+            return '/tmp/symfony/cache/' . $this->environment;
+        }
+        return parent::getCacheDir();
+    }
+
+    public function getLogDir(): string
+    {
+        if (isset($_SERVER['VERCEL']) || isset($_ENV['VERCEL'])) {
+            return '/tmp/symfony/log';
+        }
+        return parent::getLogDir();
+    }
 }
