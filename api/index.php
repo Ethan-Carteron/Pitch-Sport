69cc7576777e32915c01d9a6069256e3706a16d9
EthanCarteron <ethan.carteron@gmail.com>
2026-06-16 15:50:10 +0200

    refacto: sonarqube alerts require/require_once

--- api/index.php
+++ api/index.php
@@ -3,4 +3,4 @@
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = 'prod';
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = 0;
 $_SERVER['VERCEL'] = $_ENV['VERCEL'] = 1;
-return require __DIR__ . '/../public/index.php';
+return require_once __DIR__ . '/../public/index.php';
