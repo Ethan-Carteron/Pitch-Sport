d775904bc51286f95190bce7685cebdb88dd1c0d
EthanCarteron <ethan.carteron@gmail.com>
2026-06-16 11:46:21 +0200

    feat: add migration to create sessions table

---
+++ migrations/Version20260616094543.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260616094543 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE IF NOT EXISTS sessions (
+            sess_id VARCHAR(128) NOT NULL PRIMARY KEY,
+            sess_data BYTEA NOT NULL,
+            sess_time INTEGER NOT NULL,
+            sess_lifetime INTEGER NOT NULL
+        )');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE IF EXISTS sessions');
+    }
+}
