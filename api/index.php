54ffcf8ad37351137d0e64871c0c6e69b5b5b45e
EthanCarteron <ethan.carteron@gmail.com>
2026-06-18 10:16:02 +0200

    fix: resolve container boot error and vercel production runtime typeerror

--- api/index.php
+++ api/index.php
@@ -3,4 +3,5 @@
 $_SERVER['APP_ENV'] = $_ENV['APP_ENV'] = 'prod';
 $_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] = 0;
 $_SERVER['VERCEL'] = $_ENV['VERCEL'] = 1;
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
 return require_once __DIR__ . '/../public/index.php';
